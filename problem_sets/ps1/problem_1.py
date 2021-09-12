f391cb06ee9d5eae5ed2d1f2fd9491959a0ee787
lazdam <lazda.mattias@gmail.com>
2021-09-12 09:29:08 -0400

    updated problem 1

--- problem_sets/ps1/problem_1.py
+++ problem_sets/ps1/problem_1.py
@@ -1,66 +1,57 @@
 import numpy as np 
 
-def numerical_deriv(x,C,ε):
-    '''
-    Computes the 5-point derivative according to equation (5) (see attached pset1 PDF). 
-    
-    Parameters:
-    -----------
-    x: int/float
-        x-value where derivative is being evaluated at
-    ε_machine: float
-        ε of machine.
-    
-    Returns: Estimated derivative (float)
-    '''
-    
-    #Compute δ (see equation (7))
-    δ = ε**(1/5)
-    temp = x - δ
-    δ = x - temp
+#Part b): 
 
+#NOTE: For the sake of this code, I have re-labelled \delta as h.
 
-    #Compute x vals where f is evaluated at
-    deriv = (8*func(C, x + δ) - 8*func(C, x - δ) - func(C, x + 2*δ) + func(C, x - 2*δ))/(8*δ)
-
-    return deriv     
-
-def func(C, x):
-    '''
-    Evaluates exp(C*x) at x.
-
-    Parameters: 
-    -----------
-    C: float
-        Steepness of exponential function. 
-    x: int/float
-        Input x value
-
-    Returns: float
-    '''    
-    return np.exp(C*x) 
-
-#--------------------------------------------#	
-
-ε = 2**-52
-
-#Set x value to evaluate derivative at:
+#Set default parameters that will be the same for both functions
+eps_machine = 2**-52
 x = 42
 
-#Set array of C values requested in assignment.
-C_arr = [1]
+#1) f(x) = exp(x)
 
-deriv_arr = np.empty(len(C_arr)) #initialize empty array to store computed derivatives
+h = eps_machine**(1/5) #Since f/f^(5) = 1 
 
-for i, C in enumerate(C_arr):
-    
-    deriv = numerical_deriv(x, C, ε)
-    deriv_arr[i] = deriv
+#Compute the derivative according to equation (5) in attached PDF. Four function evals:
 
-#print(deriv_arr)
-#print(deriv_arr[0]/np.exp(x) - 1)
-#print(ε**(4/5))
+f0 = np.exp(x)
+f1 = 8*np.exp(x + h)
+f2 = -8*np.exp(x - h)
+f3 = -1*np.exp(x + 2*h)
+f4 = np.exp(x - 2*h)
+
+deriv = (f1 + f2 + f3 + f4)/(12*h)
+fractional_err_theory = eps_machine**(4/5)
+
+print('''
+f(x) = exp(x):
+--------------
+
+Derivative at x = {0} is {1} with fractional error {2}.
+The theoretical fractional accuracy of the computed derivative is ~ {3}\n
+    '''.format(x, deriv, np.exp(x)/deriv - 1, fractional_err_theory))
 
 
+#2) f(x) = exp(0.01x)
 
+h_new = (eps_machine**(1/5))*100 #since (f/f^(5))^(1/5) = (f/((0.01^5)*(f))^(1/5) = 1/0.01
+x = 4200 #scaled up, should have the same derivative
 
+#Compute the derivative according to equation (5) in attached PDF. Four function evals:
+
+f0 = np.exp(0.01*x)
+f1 = 8*np.exp(0.01*(x + h_new))
+f2 = -8*np.exp(0.01*(x - h_new))
+f3 = -1*np.exp(0.01*(x + 2*h_new))
+f4 = np.exp(0.01*(x - 2*h_new))
+
+deriv_new = (f1 + f2 + f3 + f4)/(12*h_new)
+fractional_err_theory = eps_machine**(4/5) #stays the same 
+
+print('''
+f(x) = exp(0.01x):
+--------------
+
+Derivative at x = {0} is {1} with fractional error {2}.
+The theoretical fractional accuracy of the computed derivative is {3}\n
+    '''.format(x, deriv_new, f0/deriv_new - 1, fractional_err_theory))
