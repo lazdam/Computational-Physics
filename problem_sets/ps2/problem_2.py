5552d9def853f3c08e9d52f357bbcce2ce2796a9
lazdam <mattias.lazda@mail.mcgill.ca>
2021-09-23 22:40:06 -0400

    added comments on problem 2

--- problem_sets/ps2/problem_2.py
+++ problem_sets/ps2/problem_2.py
@@ -1,6 +1,26 @@
 import numpy as np
 
 def integrate_adaptive(fun, a, b, tol, extra = None):
+    '''
+    Adaptive integrator for a function that does not repeat function evaluations on previously computted values. 
+
+    Parameters:
+    -----------
+    fun: function, callable
+        Function you'd like to integrate
+    a: float
+        Lower bount of integration
+    b: float
+        Upper bound of integration
+    tol: float
+        Error tolerance between successive Simpson's areas.  
+
+    extra: bool
+        Is none only for first function call. For recursive calls, extra = ndarray containing saved data points. 
+
+    Returns: Integral from a to b. 
+
+    '''
     
     if type(extra) == type(None): 
 
@@ -9,19 +29,20 @@ def integrate_adaptive(fun, a, b, tol, extra = None):
         x = np.linspace(a,b,5)
         y = fun(x)
         
+        #Code from class
         dx=(b-a)/(len(x)-1)
         area1=2*dx*(y[0]+4*y[2]+y[4])/3 #coarse step
         area2=dx*(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3 #finer step
         err=np.abs(area1-area2)
         
         saved_data = np.vstack((x,y)) #Stores previously computted x and y values. 
-                                      #If error isn't below tolerance level, this data will be carried over to the next cycle. 
+                                      #If error isn't below tolerance level, this data will be carried over to the next function call. 
         
         global new_counter #Counts number of function calls for improved adaptive integrator
         global old_counter #Counts number of function calls for old adaptive integrator seen in class 
 
         new_counter+=len(x) #Increase the number of new function calls
-        old_counter+=len(x) #Increase the number of old function calls (recalling that each time, it always computted fun(x) == len(x) function calls)
+        old_counter+=len(x) #Increase the number of old function calls (recalling that each time, it always computted fun(x) ==> len(x) function calls)
 
         
         if err<tol: 
@@ -34,14 +55,16 @@ def integrate_adaptive(fun, a, b, tol, extra = None):
             
             return left + right
     
+    #For subsequent recursive calls
     else: 
         
-        #Extract saved x and y values
+        #Extract saved x and y values from previous function calls
         saved_data = extra
         saved_x = saved_data[0] 
         saved_y = saved_data[1]
 
         
+        #Generate x values as usual. Need to fill up y array with either new or old values. 
         x = np.linspace(a,b,5)
         y = np.empty(len(x))
 
@@ -67,7 +90,7 @@ def integrate_adaptive(fun, a, b, tol, extra = None):
 
         saved_data = np.vstack((saved_x, saved_y)) #stack new saved x and y values for next loop, if needed
         
-        #Now that we've got our x and y values, we can compute error as normal
+        #Now that we've got our x and y values, we can compute areas and error as before
         dx=(b-a)/(len(x)-1)
         area1=2*dx*(y[0]+4*y[2]+y[4])/3 #coarse step
         area2=dx*(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3 #finer step
