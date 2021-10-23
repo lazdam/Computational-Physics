898d6d0f8c18524c54eced8d892fcf0dab039fee
lazdam <mattias.lazda@mail.mcgill.ca>
2021-10-23 17:21:35 -0400

    added problem 1

---
+++ problem_sets/ps5/problem_1.py
@@ -0,0 +1,41 @@
+'''
+Problem Set #5, Problem #1
+Mattias Lazda
+260845451
+Last Updated: October 23, 2021
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def shift_arr(arr, shift_frames):
+
+    # Define functions to convolve
+    f = arr
+    centr = len(f)//2
+
+    # Define delta function
+    g = np.zeros(len(f))
+    g[shift_frames] = 1
+
+    #Convolve functions
+    F = np.fft.rfft(f)
+    G = np.fft.rfft(g)
+    shifted_func = np.fft.irfft(F*G)
+
+    return shifted_func
+
+
+x = np.linspace(-5,5,1000)
+y = np.exp(-0.5*(x)**2/0.35**2)
+num_frames = int(len(y)/2)
+y_shifted = shift_arr(y, num_frames)
+
+if True: 
+    plt.plot(x, y, label = 'Original')
+    plt.plot(x, np.abs(y_shifted), label = 'Shifted')
+    plt.legend(loc='upper right')
+    plt.savefig('figures/gauss_shift.png')
+    plt.show()
+
+
---
+++ problem_sets/ps5/problem_2.py
@@ -0,0 +1,10 @@
+'''
+Problem Set #5, Problem #2
+Mattias Lazda
+260845451
+Last Updated: October 23, 2021
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
